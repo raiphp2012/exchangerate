650e9a47f5ce3fe94b39e1adc808c9a3947c5c22
Rakesh Rai <rakeshrai05@gmail.com>
2023-04-19 08:10:51 +0530

    create migrate  exchange rates

---
+++ database/migrations/2023_04_19_021003_create_exchange_rates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExchangeRatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exchange_rates', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->double('rate', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exchange_rates');
+    }
+}
